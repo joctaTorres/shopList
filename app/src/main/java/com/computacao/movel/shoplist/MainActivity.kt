996312e3b2dff879ce13b3c882ea8705b28fca65
Jocta <jocta.torres@gmail.com>
2019-04-21 20:16:50 -0300

    Functional camera

--- app/src/main/java/com/computacao/movel/shoplist/MainActivity.kt
+++ app/src/main/java/com/computacao/movel/shoplist/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.computacao.movel.shoplist
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val CAMERA_REQUEST_CODE = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraBtn.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            if (cameraIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    groceryImage.setImageBitmap(
+                        data.extras.get("data") as Bitmap
+                    )
+                }
+            }
+
+           else -> {
+               Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_SHORT).show()
+           }
+        }
     }
 }
