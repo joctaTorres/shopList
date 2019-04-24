73ba0802a38a9cde2746c68a1ce7bd51865cc2f5
Jocta <jocta.torres@gmail.com>
2019-04-24 18:48:52 -0300

    renaming Activity2 to startScreen

--- app/src/main/java/com/computacao/movel/shoplist/startScreen.kt
+++ app/src/main/java/com/computacao/movel/shoplist/startScreen.kt
@@ -5,18 +5,18 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_2.*
 
-class Activity2 : AppCompatActivity() {
+class startScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
 
         button.setOnClickListener {
-            val intentProcurarListasCriadas = Intent(this, Activity2::class.java) /*Trocar activity*/
+            val intentProcurarListasCriadas = Intent(this, startScreen::class.java) /*Trocar activity*/
             startActivity(intentProcurarListasCriadas)
         }
         button2.setOnClickListener {
-            val intentCriarNovaLista = Intent(this, Activity2::class.java)       /*Trocar activity*/
+            val intentCriarNovaLista = Intent(this, startScreen::class.java)       /*Trocar activity*/
             startActivity(intentCriarNovaLista)
         }
     }
