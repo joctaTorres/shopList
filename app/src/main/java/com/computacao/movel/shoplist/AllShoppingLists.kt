e04ea08b70cc2b067d9322d5286ce8bf09cc9e5a
Jocta <jocta.torres@gmail.com>
2019-05-01 19:41:50 -0300

    adding AllShoppingLists activity

---
+++ app/src/main/java/com/computacao/movel/shoplist/AllShoppingLists.kt
@@ -0,0 +1,12 @@
+package com.computacao.movel.shoplist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AllShoppingLists : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_shopping_lists)
+    }
+}
